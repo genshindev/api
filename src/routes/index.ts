ffbb7b46ae910b0d12e67146e247145e44e1cb51
Marvin Witt <nurmarvin@protonmail.com>
2021-03-27 13:47:31 +0100

    feat: add filter query parameters
    Usage: append i.e. `?type=Catalyst` to the `/all` sub route of an entity and then only the entities which's field with the key `type` is equal to `Catalyst`. Those query parameters can be chained, so if you e.g. wanted all weapons with a rarity of `3` and type `Bow`, then you would use `?type=Bow&rarity=3`. 
     
    Closes #43

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -37,17 +37,34 @@ router.get('/:type', async (ctx) => {
 
 router.get('/:type/all', async (ctx) => {
   try {
-    const { lang } = ctx.query;
+    const { lang, ...params } = ctx.query;
     const { type } = ctx.params;
     const entities = await getAvailableEntities(type);
 
     if (!entities) return;
 
-    ctx.body = await Promise.all(
+    const entityObjects = await Promise.all(
       entities.map(async (id) => {
         return await getEntity(type, id, lang);
       }),
     );
+
+    ctx.body = entityObjects.filter((entity) => {
+      for (const key of Object.keys(params)) {
+        const value = entity[key];
+
+        switch (typeof value) {
+          case 'string':
+            if (!value.includes(params[key])) return false;
+            break;
+          default:
+            if (value != params[key]) return false;
+            break;
+        }
+      }
+
+      return true;
+    });
   } catch (e) {
     ctx.status = 404;
     ctx.body = { error: e.message };
